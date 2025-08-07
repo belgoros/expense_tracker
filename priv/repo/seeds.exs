c3317e43f62cafb3bffd2f4f07cece7af3bb82e7
belgoros <s.cambour@gmail.com>
2025-08-07 17:42:26 +0200

    Create seeds to populate the DB

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,46 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+defmodule Seeder do
+  alias ExpenseTracker.Repo
+  alias ExpenseTracker.Budget
+  alias ExpenseTracker.Budget.{Category, Expense}
+
+  def run() do
+    Repo.delete_all(Category)
+
+    create_categories()
+    |> Enum.each(fn category ->
+      create_expense_for(category)
+    end)
+  end
+
+  defp create_categories do
+    for _ <- 1..5 do
+      %Category{
+        description: Faker.Food.dish(),
+        monthly_budget: Enum.random(100..500),
+        name: Faker.Commerce.department()
+      }
+      |> Repo.insert!()
+    end
+  end
+
+  defp create_expense_for(category) do
+    Enum.each(
+      1..3,
+      fn _ ->
+        %Expense{
+          category_id: category.id,
+          date: Faker.Date.backward(4),
+          description: Faker.Commerce.En.product_name(),
+          amount: Enum.random(2..50),
+          notes: Faker.Lorem.sentence()
+        }
+        |> Repo.insert!()
+      end
+    )
+  end
+end
+
+Seeder.run()
