a88a43bf6325eb4e07d39df869f4a3eb559105d7
belgoros <s.cambour@gmail.com>
2025-08-07 09:25:08 +0200

    Add and setup faker

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule ExpenseTracker.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:faker, "~> 0.18.0", only: [:dev, :test]}
     ]
   end
 
